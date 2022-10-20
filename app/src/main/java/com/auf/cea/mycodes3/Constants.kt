e7483913c83b89354e28267403d6e9b86cabbd40
gabdizon <dizon.christiangabriel@auf.edu.ph>
2022-10-20 21:27:21 +0800

    edit xml

---
+++ app/src/main/java/com/auf/cea/mycodes3/Constants.kt
@@ -0,0 +1,4 @@
+package com.auf.cea.mycodes3
+
+const val MY_PREFERENCE = "MY_PREFERENCE"
+const val USERNAME = "KEY_USER_NAME"
--- app/src/main/java/com/auf/cea/mycodes3/SplashScreenActivity.kt
+++ app/src/main/java/com/auf/cea/mycodes3/SplashScreenActivity.kt
@@ -1,11 +1,43 @@
 package com.auf.cea.mycodes3
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import com.auf.cea.mycodes3.databinding.ActivitySplashScreenBinding
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySplashScreenBinding
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        val sharedPreferences = getSharedPreferences("MY_PREFERENCES", Context.MODE_PRIVATE)
+
+        val username = sharedPreferences.getString(USERNAME,"").toString()
+
+        if (username == ""){
+            binding.txtWelcomeText.text = "WELCOME TO MY APPLICATION!"
+        } else{
+            binding.txtWelcomeText.text = "WELCOME TO MY APPLICATION, \n $username";
+        }
+
+        object : CountDownTimer(5000,1000){
+            override fun onTick(p0: Long) {
+            }
+            override fun onFinish() {
+                val intent = Intent(this@SplashScreenActivity,MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+            }
+        }.start()
     }
+
 }
